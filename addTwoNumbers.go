1e96d81935e104cbec55a362543504d4c1c1d8ec
= <=>
2017-04-22 14:45:13 +0800

    git config & addTwoNumber fix

--- addTwoNumbers.go
+++ addTwoNumbers.go
@@ -5,12 +5,34 @@ Output: 7 -> 0 -> 8
 
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
 func main() {
+	var (
+		a1 = &ListNode{1, nil}
+		a2 = &ListNode{2, nil}
+		a3 = &ListNode{3, nil}
+		b1 = &ListNode{4, nil}
+		b2 = &ListNode{5, nil}
+		b3 = &ListNode{9, nil}
+	)
+
+	a1.Next = a2
+	a2.Next = a3
+
+	b1.Next = b2
+	b2.Next = b3
+
+	res := addTwoNumbers(a1, b1)
+	for res != nil {
+		fmt.Println("%d", res.Val)
+		res = res.Next
+	}
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
@@ -51,7 +73,6 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			end.Next = node
 			end = node
 		}
-
 	}
 
 	if flag > 0 {
